193d8138ce0f06e3fabcaf1a0fc5eaf5a20b771d
Chandru <tochandru.engineer@gmail.com>
2022-06-26 09:33:24 +0530

    User Login Added

--- CREDproject/base/urls.py
+++ CREDproject/base/urls.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
 from django.urls import path
+from . import views
 from .views import TaskList
 from .views import TaskDetails
 from .views import TaskCreate
 from .views import TaskUpdate
 from .views import TaskDelete
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    path('logout/', LogoutView.as_view(next_page = 'login'), name = 'logout'),
+    path('login/', views.CustomLoginView.as_view(), name= 'login'),
     path('', TaskList.as_view(), name='to-do-list'),
     path('task/<int:pk>/', TaskDetails.as_view(), name='task'),
     path('task-create/', TaskCreate.as_view(), name='task-create'),
--- CREDproject/base/views.py
+++ CREDproject/base/views.py
@@ -1,34 +1,56 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Task
 
+from django.contrib.auth.views import LoginView
 
-class TaskList(ListView):
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('to-do-list')
+
+
+class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'tasks'
 
-class TaskDetails(DetailView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasts'] = context['tasks'].filter(user = self.request.user)
+        context['count'] = context['tasks'].filter(complete = False).count()
+        return context
+
+class TaskDetails(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'base/task.html'
 # Create your views here.
 
-class TaskCreate(CreateView):
+class TaskCreate(LoginRequiredMixin, CreateView):
+    model = Task
+    fields = ['title', 'discription', 'complete']
+    success_url = reverse_lazy('to-do-list')
+
+    def form_invalid(self, form):
+        form.instance.user = self.request.user
+        return super(TaskCreate, self).form_valid(form)
+
+class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('to-do-list')
 
-class TaskUpdate(UpdateView):
-    model = Task
-    fields = '__all__'
-    success_url = reverse_lazy('to-do-list')
 
-
-class TaskDelete(DeleteView):
+class TaskDelete(LoginRequiredMixin, DeleteView):
     model = Task
     context_object_name = 'task'
     success_url = reverse_lazy('to-do-list')
